0a91c2b821856b0610520bf68407b0b732e12a90
DK <kuryazov@se.uni-oldenburg.de>
2020-01-08 21:23:08 +0500

    #21 Item Controller using API approach is created

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Http\Requests;
 use App\Http\Controllers\Controller;
+
+use App\Item;
+use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
@@ -12,53 +15,70 @@ class ItemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $item = Item::latest()->paginate(25);
+
+        return $item;
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        
+        $item = Item::create($request->all());
+
+        return response()->json($item, 201);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        $item = Item::findOrFail($id);
+
+        return $item;
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @param  int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        
+        $item = Item::findOrFail($id);
+        $item->update($request->all());
+
+        return response()->json($item, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        Item::destroy($id);
+
+        return response()->json(null, 204);
     }
 }
