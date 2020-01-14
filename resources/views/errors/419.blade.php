2b26d38419085e8996b89e192f4390735183dcaa
javlonbek-sharipov <javlonbek.sharipoff@gmail.com>
2020-01-14 11:26:03 +0500

    Fixed typo on button classes

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="very-center">
             <h4>{!! __('errors.expired') !!}</h4>
-            <a href="/" class="btn btn-danger">&laquo; {{__('driver.back')}}</a>
+            <a href="/" class="btn btn-warning">&laquo; {{__('driver.back')}}</a>
         </div>
     </div>
 @endsection
