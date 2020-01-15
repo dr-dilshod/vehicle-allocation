11690702d174d4ff552e87c2cbbfa2a951e337dc
javlonbek-sharipov <javlonbek.sharipoff@gmail.com>
2020-01-16 01:42:10 +0500

    Added Billing and Dispatch models&migrations

---
+++ database/migrations/2020_01_15_201357_create_dispatch_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDispatchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dispatch', function (Blueprint $table) {
+            $table->bigIncrements('dispatch_id');
+            $table->unsignedBigInteger('driver_id');
+            $table->unsignedBigInteger('item_id');
+            $table->tinyInteger('timezone');
+            $table->tinyInteger('delete_flg')->default(0);
+            $table->unsignedBigInteger('create_id');
+            $table->unsignedBigInteger('update_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dispatch_board');
+    }
+}
