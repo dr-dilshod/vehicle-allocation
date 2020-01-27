8656c762f739529039a40d798a41f6bf0c38a895
N0D1R <n.sadullayev@gmail.com>
2020-01-27 11:59:28 +0500

    Simple Test cases for Deposit CRUD API

--- tests/Feature/Api/DepositApiTest.php
+++ tests/Feature/Api/DepositApiTest.php
@@ -2,21 +2,70 @@
 
 namespace Tests\Feature\Api;
 
+use App\Deposit;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class DepositApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use WithoutMiddleware;
+
+    public function testIndex()
     {
-        $response = $this->get('/');
+        $response = $this->json('GET', route('api.deposit.index'));
+        $response
+            ->assertStatus(200)
+            ->assertJson([])
+            ->assertJsonStructure([
+                'data' => [
+                    ['deposit_id','invoice_id', 'create_id', 'update_id', 'deposit_amount', 'fee', 'deposit_remark', 'delete_flg', 'created_at', 'updated_at']
+                ]
+            ]);
+    }
+
+    public function testCreate(){
+
+        $deposit = factory(Deposit::class)->make();
+
+        $response = $this->json('POST', route('api.deposit.store'),
+            $deposit->toArray());
+
+        $response->assertStatus(201)
+                    ->assertJsonStructure([
+                        'deposit_id','invoice_id', 'create_id', 'update_id', 'deposit_amount', 'fee', 'deposit_remark', 'delete_flg', 'created_at', 'updated_at'
+                    ]);
+    }
+
+    public function testShow(){
+
+        $deposit = factory(Deposit::class)->create();
+
+        $response = $this->json('GET', route('api.deposit.show', [$deposit->deposit_id]));
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'deposit_id','invoice_id', 'create_id', 'update_id', 'deposit_amount', 'fee', 'deposit_remark', 'delete_flg', 'created_at', 'updated_at'
+            ]);
+    }
+
+    public function testUpdate(){
+
+        $id = factory(Deposit::class)->create()->deposit_id;
+        $deposit = factory(Deposit::class)->make();
+
+        $response = $this->json('PUT', route('api.deposit.update',[$id]), $deposit->toArray());
 
         $response->assertStatus(200);
     }
+
+    public function testDelete(){
+
+        $id = factory(Deposit::class)->create()->deposit_id;
+
+        $response = $this->json('DELETE',route('api.deposit.destroy',[$id]));
+
+        $response->assertStatus(204);
+    }
+
 }
