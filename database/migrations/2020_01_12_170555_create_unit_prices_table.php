f7757fc66a9393765779ba86250ef46b00b81fd1
javlonbek-sharipov <javlonbek.sharipoff@gmail.com>
2020-01-30 03:17:57 +0500

    UnitPrice CRUD

--- database/migrations/2020_01_12_170555_create_unit_prices_table.php
+++ database/migrations/2020_01_12_170555_create_unit_prices_table.php
@@ -16,8 +16,8 @@ class CreateUnitPricesTable extends Migration
         Schema::create('unit_prices', function (Blueprint $table) {
             $table->bigIncrements('price_id');
             $table->unsignedBigInteger('shipper_id');
-            $table->unsignedBigInteger('item_id');
-            $table->unsignedBigInteger('driver_id');
+            $table->unsignedBigInteger('item_id')->nullable();
+            $table->unsignedBigInteger('driver_id')->nullable();
             $table->tinyInteger('type');
             $table->unsignedBigInteger('price');
             $table->tinyInteger('delete_flg')->default(0);
@@ -25,12 +25,12 @@ class CreateUnitPricesTable extends Migration
             $table->unsignedBigInteger('update_id')->nullable();
             $table->timestamps();
 
-            $table->index(['create_id', 'update_id', 'shipper_id', 'item_id', 'driver_id'], 'unit_prices-default-indexes');
+            $table->index(['create_id', 'update_id', 'shipper_id', /*'item_id', 'driver_id'*/], 'unit_prices-default-indexes');
             $table->foreign('create_id')->references('driver_id')->on('drivers')->onDelete('restrict')->onUpdate('restrict');
             $table->foreign('update_id')->references('driver_id')->on('drivers')->onDelete('restrict')->onUpdate('restrict');
             $table->foreign('shipper_id')->references('shipper_id')->on('shippers')->onDelete('restrict')->onUpdate('restrict');
-            $table->foreign('item_id')->references('item_id')->on('items')->onDelete('restrict')->onUpdate('restrict');
-            $table->foreign('driver_id')->references('driver_id')->on('drivers')->onDelete('restrict')->onUpdate('restrict');
+//            $table->foreign('item_id')->references('item_id')->on('items')->onDelete('restrict')->onUpdate('restrict');
+//            $table->foreign('driver_id')->references('driver_id')->on('drivers')->onDelete('restrict')->onUpdate('restrict');
         });
     }
 
