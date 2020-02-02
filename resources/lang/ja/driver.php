5f105b357eb7dee6d5cebaca08a354fbd7fd196c
Muzaffar Artikov <artikovmuzaffar@gmail.com>
2020-02-02 17:36:15 +0500

    Lang directory & files

---
+++ resources/lang/ja/driver.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Muzaffar
+ * Date: 24.12.2019
+ * Time: 17:18
+ */
+return [
+    'driver_list' => 'Driver list',
+    'type' => 'Type',
+    'name' => 'Name',
+    'mobile_number' => 'Mobile number',
+    'vehicle_no' => 'Vehicle No',
+    'max_load' => 'Max Load',
+    'display' => 'Display',
+    'remarks' => 'Remarks'
+];
---
+++ resources/lang/ja/errors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'not_found' => 'Requested page not found.',
+    'unauthorized' => 'You are not logged in.',
+    'forbidden' => 'Access to this page is forbidden.',
+    'expired' => 'Your session has expired.<br/> Please refresh and try again.',
+    'too_many_requests' => 'Too many requests.',
+    'server_error' => 'The server encountered an error.',
+    'service_unavailable' => 'Service unavailable.'
+];
