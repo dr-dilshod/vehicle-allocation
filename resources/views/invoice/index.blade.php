72c2f914644b387c96da342f8ccd2579c065d209
DK <kuryazov@se.uni-oldenburg.de>
2020-02-04 02:31:35 +0500

    #28 Calculations and billing month PDF feature

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -4,10 +4,12 @@
     <div class="container">
         <invoice invoice-url="{{route('invoice.list')}}"
                  back-url="{{route('top')}}"
-                 shippers-url="{{route('api.shipper.distinct-name')}}"
+                 shippers-url="{{route('shipper.list')}}"
                  vehicles-url="{{route('api.driver.vehicle-numbers')}}"
                  resource-url="/api/invoice"
                  title="Invoice list"
+                 payment-url="{{route('payment.list')}}"
+                 deposit-url="{{route('deposit.list')}}"
         ></invoice>
     </div>
 @endsection
