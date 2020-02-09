a3efbe7ef63549072896286b25df53f313c32c20
javlonbek-sharipov <javlonbek.sharipoff@gmail.com>
2020-02-09 18:40:37 +0500

    Hard coded url fixed

--- resources/views/unit-price/index.blade.php
+++ resources/views/unit-price/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <unit-price-table
-            resource-url="{{route('api.unit-prices.resource')}}"
+            resource-url="{{route('api.unit-prices.index')}}"
             back-url="{{route('setting')}}"
             title="{{__('unit_prices.unit_price_list')}}"
         ></unit-price-table>
