cc78645b619f338d006a0b5b54fc478e255261ef
N0D1R <n.sadullayev@gmail.com>
2020-02-09 20:17:46 +0500

    Grid localization

---
+++ resources/js/utils/syncfusion-local.js
@@ -0,0 +1,36 @@
+/**
+ * Created by N0D1R on 09-Feb-20.
+ */
+import { L10n, setCulture } from '@syncfusion/ej2-base';
+
+setCulture('ja-JA');
+// setCulture('en-EN');
+// setCurrencyCode('EUR');
+/**
+ * Visit for more properties
+ * https://help.syncfusion.com/js/grid/globalizationandlocalization
+ */
+
+L10n.load({
+    'ja-JA': {
+        'grid': {
+            'SaveButton' : 'Saqlash',
+            'CancelButton' : 'Bekor',
+            'OKButton' : 'Ho\'p',
+            'Save' : 'Saqlash',
+            'Add' : 'Qo\'shish',
+            'Edit' : 'Tahrirlash',
+            'Delete' : 'O\'chirish',
+            'Update' : 'Saqlash',
+            'Exit' : 'Chiqish',
+            'Cancel' : 'Bekor qilish',
+            'Done' : 'Bajarildi',
+            'EmptyRecord': 'Ma`lumot topilmadi',
+            'ConfirmDelete' : 'O`chirasama',
+            'DeleteOperationAlert' : 'O\'chirish uchun qatorni belgilang',
+            'EditOperationAlert' : 'Tahrirlash uchun qatorni belgilang',
+            'CancelEdit' : 'Tahrirlashni bekor qilishni xohlaysizmi ?',
+            'NoResult' : 'Ma\'lumot yo\'q',
+        },
+    }
+});
