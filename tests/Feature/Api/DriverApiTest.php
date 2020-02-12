07b07fc04ae611b1912e627b60c2cc0ebcbe3fbd
Bekmurod <bekmurod3388@gmail.com>
2020-02-12 13:57:10 +0500

    driver api test

--- tests/Feature/Api/DriverApiTest.php
+++ tests/Feature/Api/DriverApiTest.php
@@ -10,8 +10,30 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DriverApiTest extends TestCase
 {
+    use WithFaker;
     use WithoutMiddleware;
-    /**
+    private $driver;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->driver = [
+            'driver_pass' => 'DriverPass',
+            'driver_name' => 'DriverName1',
+            'driver_mobile_number' => '99998888899',
+            'maximum_Loading' => '77',
+            'search_flg' => '1',
+            'admin_flg' => '1',
+            'vehicle_no1' => '10',
+            'vehicle_no2' => '555',
+            'vehicle_no3' => '666',
+            'vehicle_type' => 'Blank',
+            'driver_remark' =>'Remarks for driver list',
+            'delete_flg' => 0,
+
+            ];
+    }
+
+/**
      * A basic feature test example.
      *
      * @return void
@@ -21,30 +43,26 @@ class DriverApiTest extends TestCase
      */
     public function testDriverSchema()
     {
-        $user = Driver::findOrFail(1);
-        $response = $this
-            ->actingAs($user)
-            ->json('GET', route('api.driver.index'));
+        $response = $this->json('GET', route('api.driver.index', [1]));
+        echo $response->getContent();
         $response
-            ->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    'create_id', 'update_id', 'driver_pass', 'driver_name', 'driver_mobile_number',
-                    'maximum_Loading', 'search_flg', 'admin_flg', 'vehicle_no1', 'vehicle_no2',
-                    'vehicle_no3', 'vehicle_type', 'driver_remark', 'delete_flg',
-                    'created_at', 'updated_at']
+
+            ->assertJsonStructure(['*'=>[
+                'driver_id', 'driver_pass', 'driver_name',
+                'driver_mobile_number', 'maximum_Loading', 'search_flg',
+                'admin_flg', 'vehicle_no1', 'vehicle_no2', 'vehicle_no3',
+                'vehicle_type', 'driver_remark', 'delete_flg', 'remember_token',
+                'create_id', 'update_id', 'created_at', 'updated_at']
             ]);
     }
     /**
      * Testing Driver creation
      */
     public function testCreateDriver(){
-
-        $driver = factory(Driver::class)->make();
-
-        $response = $this->json('POST', route('api.driver.store'), $driver->toArray());
-
+        $response = $this->json('POST', route('api.driver.store'), $this->driver);
         $response->assertStatus(201);
+        $this->assertDatabaseHas('vehicles', $this->vehicle);
+        Vehicle::where('vehicle_no', '4444')->delete();
     }
     /**
      * Testing structure of one record
