2e420bf2e02f4d067b7ed113913d6a7f2775456f
DK <kuryazov@se.uni-oldenburg.de>
2020-02-14 23:28:15 +0500

    Fixed test cases for item and payment apis

--- tests/Feature/Api/ItemApiTest.php
+++ tests/Feature/Api/ItemApiTest.php
@@ -21,11 +21,11 @@ class ItemApiTest extends TestCase
             'driver_id'=>1,
             'vehicle_id'=>1,
             'status'=>0,
-            'stack_date'=>'2020-01-01 00:00:00',
+            'stack_date'=>'2000-01-01',
             'stack_time'=>'10:00:00',
-            'down_date'=>'2020-01-01 00:00:00',
+            'down_date'=>'2000-01-01',
             'down_time'=>'20:00:00',
-            'down_invoice'=>1,
+            'down_invoice'=>0,
             'stack_point'=>'StackPointTest',
             'down_point'=>'DownPointTest',
             'weight'=>20,
@@ -36,11 +36,8 @@ class ItemApiTest extends TestCase
             'shipper_name'=>'SomeTestShipperName',
             'item_vehicle'=>'VehicleTest',
             'vehicle_payment'=>100,
-            'item_completion_date'=>'2020-01-01 00:00:00',
-            'item_remark'=>'RemarkTest',
-            'delete_flg'=>0,
-            'create_id'=>1,
-            'update_id'=>1,
+            'item_completion_date'=>'2020-01-01',
+            'item_remark'=>'ThisIsTestRemark',
             'remember_token'=>'testRememberToken',
             'created_at' =>'2020-01-01 00:00:00',
             'updated_at'=>'2020-01-01 00:00:00',
@@ -52,10 +49,12 @@ class ItemApiTest extends TestCase
      */
     public function testItemSchema()
     {
-        $response = $this->json('GET', route('api.item.index', [1]));
+        $payment = factory(\App\Item::class)->create($this->item);
+        $itemInDB = Item::where('item_remark', 'ThisIsTestRemark')
+            ->get()->first();
+        $response = $this->json('GET', route('api.item.index', [$itemInDB->item_id]));
         $response
-            ->assertStatus(200)
-            ->assertJson([])
+             ->assertStatus(200)
             ->assertJsonStructure([
                 'data' => [
                     ['shipper_id', 'driver_id', 'vehicle_id', 'status',
@@ -66,6 +65,7 @@ class ItemApiTest extends TestCase
                         'created_at', 'update_id', 'updated_at', 'remember_token']
                 ]
             ]);
+        Item::where('item_id', $itemInDB->item_id)->delete();
     }
 
     /**
@@ -74,9 +74,13 @@ class ItemApiTest extends TestCase
     public function testCreateItem(){
         $itemObject = factory(Item::class)->make($this->item);
         $response = $this->json('POST', route('api.item.store'), $this->item);
-        $response->assertStatus(201);
-        $this->assertDatabaseHas('items', $itemObject);
-        Item::where('item_id', $itemObject->item_id);
+        $response->assertStatus(200);
+        $response->assertJsonFragment(
+            $this->item);
+        $itemInDB = Item::where('item_remark', 'ThisIsTestRemark')
+            ->get()->first();
+        Item::where('item_id', $itemInDB->item_id)->delete();
+
     }
 
     /**
@@ -84,37 +88,40 @@ class ItemApiTest extends TestCase
      */
     public function testGetItem(){
         $item = factory(Item::class)->create($this->item);
-        $response = $this->json('GET', route('api.item.show', [$item->item_id]));
+        $itemInDB = Item::where('item_remark', 'ThisIsTestRemark')
+            ->get()->first();
+        $response = $this->json('GET', route('api.item.show', [$itemInDB->item_id]));
         $response->assertStatus(200)
-            ->assertJsonStructure(['shipper_id', 'driver_id', 'vehicle_id', 'status',
-                'stack_date', 'stack_time', 'down_date', 'down_time', 'down_invoice',
-                'stack_point', 'down_point', 'weight', 'empty_pl', 'item_price',
-                'item_driver_name', 'vehicle_no3', 'shipper_name', 'item_vehicle', 'vehicle_payment',
-                'item_completion_date', 'item_remark', 'delete_flg', 'create_id',
-                'created_at', 'update_id', 'updated_at', 'remember_token']);
-        $response->assertJsonFragment($item);
+            ->assertJsonFragment($this->item);
+        $response->assertSeeText($itemInDB->stack_date);
+        $response->assertSeeText($itemInDB->down_date);
+        $response->assertSeeText($itemInDB->item_remark);
+        $response->assertSeeText($itemInDB->item_driver_name);
+        Item::where('item_id', $itemInDB->item_id)->delete();
     }
 
     /**
      * Testing the update end point of Item API
      */
-    public function testUpdateShipper() {
+    public function testUpdateItem() {
+        $item = factory(Item::class)->create($this->item);
+        $itemInDB = Item::where('item_remark', 'ThisIsTestRemark')
+            ->get()->first();
         $update_data = ['status' => 1,
             'stack_point' => 'SecondStackPointTest',
             'down_point' => 'SecondDownPointTest'];
-        $new_item = factory(Item::class)->create($this->item);
-        $response = $this->json('PUT', route('api.item.update',[$new_item]),
+        $response = $this->json('PUT', route('api.item.update',[$itemInDB->item_id]),
             $update_data);
         $response->assertStatus(200);
-        $this->assertDatabaseHas('item', $update_data);
-        Shipper::where('stack_point', 'SecondStackPointTest')->delete();
+        $response->assertJsonFragment($update_data);
+        Item::where('item_id', $itemInDB->item_id)->delete();
     }
 
     /**
      * Testing the delete end point of API
      */
     public function testDeleteItem(){
-        $item = factory(Item::class)->create($this->shipper);
+        $item = factory(Item::class)->create($this->item);
         $response = $this->json('DELETE',route('api.item.destroy',[$item->item_id]));
         $response->assertStatus(204);
     }
--- tests/Feature/Api/PaymentApiTest.php
+++ tests/Feature/Api/PaymentApiTest.php
@@ -17,17 +17,33 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class PaymentApiTest extends TestCase
 {
-    use RefreshDatabase;
     use WithoutMiddleware;
     use WithFaker;
+    private $payment;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->payment = [
+            'shipper_id'=>2,
+            'payment_day' => "2000-01-01 01:01:01",
+            "payment_amount" => 3000,
+            'other' => 3000,
+            'fee' => 3000,
+            'delete_flg' => 0,
+            'created_at' => "2000-01-01 01:01:01",
+            'updated_at' => "2000-01-01 01:01:01"
 
+        ];
+    }
     /**
      * test the database schema for the result of index page
      */
     public function testPaymentSchema()
     {
-        $payment = factory(\App\Payment::class, 1)->create();
-        $response = $this->json('GET', route('api.payment.index', [$payment->payment_id]));
+        $payment = factory(\App\Payment::class)->create($this->payment);
+        $paymentInDB = Payment::where('payment_day', '2000-01-01 01:01:01')
+            ->get()->first();
+        $response = $this->json('GET', route('payment.search', [$paymentInDB->payment_id]));
         $response
             ->assertStatus(200)
             ->assertJsonStructure([
@@ -36,50 +52,60 @@ class PaymentApiTest extends TestCase
                         'fee', 'delete_flg', 'create_id', 'update_id', 'created_at', 'updated_at']
                 ]
             ]);
-        Payment::where('payment_id', $payment->payment_id)->delete();
+        Payment::where('payment_id', $paymentInDB->payment_id)->delete();
     }
 
     /**
      * Testing payment creation
      */
     public function testCreatePayment(){
-        $paymentAsArray = factory(Payment::class, 1)->raw();
-        $response = $this->json('POST', route('api.payment.store'), $paymentAsArray);
+        $response = $this->json('POST', route('api.payment.store'), $this->payment);
         $response->assertStatus(201);
-        $this->assertDatabaseHas('payments', $paymentAsArray);
-        Shipper::where('payment_id', $paymentAsArray)->delete();
+        $response->assertJsonFragment(
+            $this->payment);
+        $paymentInDB = Payment::where('payment_day', '2000-01-01 01:01:01')
+            ->get()->first();
+        Payment::where('payment_id', $paymentInDB->payment_id)->delete();
     }
 
     /**
      * Testing structure of one record and test getting one record
      */
     public function testGetPayment(){
-        $paymentAsArray = factory(Payment::class, 1)->raw();
-        $payment = factory(Payment::class, 1)->create($paymentAsArray);
-        $response = $this->json('GET', route('api.payment.show', [$payment->payment_id]));
+        $payment = factory(Payment::class)->create($this->payment);
+        $paymentInDB = Payment::where('payment_day', '2000-01-01 01:01:01')
+            ->where('created_at', "2000-01-01 01:01:01")
+            ->where('updated_at', "2000-01-01 01:01:01")
+            ->get()->first();
+        $response = $this->json('GET', route('api.payment.show', [$paymentInDB->payment_id]));
         $response->assertStatus(200)
-            ->assertJsonFragment($paymentAsArray);
-        $response->assertSeeText($paymentAsArray->payment_amount);
-        $response->assertSeeText($paymentAsArray->other);
-        $response->assertSeeText($paymentAsArray->fee);
-        $response->assertSeeText($paymentAsArray->payment_day);
+            ->assertJsonFragment($this->payment);
+        $response->assertSeeText($paymentInDB->payment_amount);
+        $response->assertSeeText($paymentInDB->other);
+        $response->assertSeeText($paymentInDB->fee);
+        $response->assertSeeText($paymentInDB->payment_day);
+        Payment::where('payment_id', $paymentInDB->payment_id)->delete();
     }
 
     /**
      * Testing the update end point of Payment API
      */
     public function testUpdatePayment() {
-        $paymentAsArray = factory(Payment::class, 1)->raw();
-        $update_data = ['payment_day'=>'2020-02-02 00:00:00',
+        $paymentAsArray = factory(Payment::class)->create($this->payment);
+        $paymentInDB = Payment::where('payment_day', '2000-01-01 01:01:01')
+            ->where('created_at', "2000-01-01 01:01:01")
+            ->where('updated_at', "2000-01-01 01:01:01")
+            ->get()->first();
+        $update_data = ['shipper_id'=>2,
+            'payment_day'=>'2020-02-02 00:00:00',
             'payment_amount'=>2001,
             'other'=>1001,
             'fee'=>1001];
-        $new_payment = factory(Payment::class)->create($paymentAsArray);
-        $response = $this->json('PUT', route('api.payment.update',[$new_payment->payment_id]),
+        $response = $this->json('PUT', route('api.payment.update',[$paymentInDB->payment_id]),
             $update_data);
         $response->assertStatus(200);
-        $this->assertDatabaseHas('payments', $update_data);
-        Payment::where('payment_id', $paymentAsArray->payment_id)->delete();
+        $response->assertJsonFragment($update_data);
+        Payment::where('payment_id', $paymentInDB->payment_id)->delete();
     }
 
 
@@ -87,11 +113,16 @@ class PaymentApiTest extends TestCase
      * Testing the delete end point of API
      */
     public function testDeletePayment(){
+        $payment = factory(Payment::class, 1)->create($this->payment);
+        $paymentInDB = Payment::where('payment_day', '2000-01-01 01:01:01')
+            ->where('created_at', "2000-01-01 01:01:01")
+            ->where('updated_at', "2000-01-01 01:01:01")
+            ->get()->first();
 
-        $payment = factory(Payment::class, 1)->create();
-        $response = $this->json('DELETE',route('api.payment.destroy',[$payment->payment_id]));
+        $response = $this->json('DELETE',route('api.payment.destroy',[$paymentInDB->payment_id]));
         $response->assertStatus(204);
-        $res = Payment::where('payment_id', $payment->payment_id)
+        $res = Payment::where('payment_id', $paymentInDB->payment_id)
+            ->where('delete_flg', 0)
             ->count();
         self::assertEquals(0, $res);
     }
