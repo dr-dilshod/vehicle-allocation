d3276b6a341f0cae31e3241966ebf6352b010f39
Muzaffar Artikov <artikovmuzaffar@gmail.com>
2020-02-16 21:15:37 +0100

    login page UI corrections

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,53 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-               <h3 class="offset-md-6 pl-3">{{ __('common.login') }}</h3>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-6 offset-3">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                <h3 class="text-center m-4">{{ __('common.login') }}</h3>
 
-                        <div class="form-group row">
-                            <label for="driver_name" class="col-md-4 col-form-label text-md-right">{{ __('common.username') }}</label>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                            <div class="col-md-6">
-                                <input id="driver_name" type="text" class="form-control @error('driver_id') is-invalid @enderror" name="driver_id" value="{{  old('driver_id') }}" required autocomplete="driver_id" autofocus>
+                    <div class="form-group row">
+                        <label for="driver_name"
+                               class="col-md-4 col-form-label text-md-right">{{ __('common.username') }}</label>
 
-                                @error('driver_id')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="col-md-6">
+                            <input id="driver_name" type="text"
+                                   class="form-control @error('driver_id') is-invalid @enderror" name="driver_id"
+                                   value="{{  old('driver_id') }}" required autocomplete="driver_id" autofocus>
+
+                            @error('driver_id')
+                            <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('common.password') }}</label>
+                    <div class="form-group row">
+                        <label for="password"
+                               class="col-md-4 col-form-label text-md-right">{{ __('common.password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <div class="col-md-6">
+                            <input id="password" type="password"
+                                   class="form-control @error('password') is-invalid @enderror" name="password"
+                                   required autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-6">
-                                <button name="login" type="submit" class="btn btn-warning btn-lg">
-                                    {{ __('common.login') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    <div class="form-group mt-4">
+                        <p class="text-center mt-3">
+                            <button name="login" type="submit" class="btn btn-warning btn-lg">
+                                {{ __('common.login') }}
+                            </button>
+                        </p>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 @endsection
