42a6afb99c789c788f31f4019d7a177db72b44e8
Muzaffar Artikov <artikovmuzaffar@gmail.com>
2020-02-19 20:07:27 +0100

    Back url fix on errors

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="very-center">
             <h4>{!! __('errors.service_unavailable') !!}</h4>
-            <a href="{{Url::to('/')}}" class="btn btn-warning">&laquo; {{__('common.back')}}</a>
+            <a href="{{route('top')}}" class="btn btn-warning">&laquo; {{__('common.back')}}</a>
         </div>
     </div>
 @endsection
