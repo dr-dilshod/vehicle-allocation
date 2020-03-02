b231dacaabee81ee9eec8d18b500a66434afe0f6
Bekmurod <bekmurod3388@gmail.com>
2020-03-02 22:22:54 +0500

    item validation translated

--- resources/lang/ja/common.php
+++ resources/lang/ja/common.php
@@ -26,7 +26,7 @@ return  [
     'ok'=>'オーケー',
     'add'=>'追加',
     'done'=>'やった',
-    'empty_record'=>'空のレコード',
+    'empty_record'=>'データがありません',
     'confirm_delete'=>'消去してもよろしいですか？',
     'delete_operation_alert'=>'行を選択して削除します',
     'edit_operation_alert'=>'編集する行を選択します',
