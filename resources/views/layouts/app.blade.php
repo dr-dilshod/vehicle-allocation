03d06b08b9e1786562588d7d188a65954434dd49
Muzaffar Artikov <artikovmuzaffar@gmail.com>
2020-03-17 16:00:42 +0100

    Bug no. 102 fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,33 +22,36 @@
 </head>
 <body>
     <div id="app">
-        <div class="container">
+        <div class="container-fluid">
             <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ __('common.sys_name')  }}
-                        </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ __('common.sys_name')  }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <!-- Left Side Of Navbar -->
-                            <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-                            </ul>
+                    </ul>
 
-                            <!-- Right Side Of Navbar -->
-                            <!-- Authentication Links -->
-                            @guest
+                    <!-- Right Side Of Navbar -->
+                    <!-- Authentication Links -->
+                    @guest
 
-                            @else
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <button type="submit" class="btn btn-warning btn-lg btn-fixed-width">{{__('common.logout')}}</button>
-                                </form>
-                            @endguest
-                        </div>
-                    </nav>
+                    @else
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit"
+                                class="btn btn-warning btn-lg btn-fixed-width">{{__('common.logout')}}</button>
+                    </form>
+                    @endguest
+                </div>
+            </nav>
         </div>
 
         <main class="py-4">
